4f3ffb1921868bd9b7378a30fb6186532171cb9e
wimuweb <wimuweb@gmail.com>
2020-09-08 14:42:46 -0500

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin =require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+
+    mode: 'development', //production
+    optimization: {
+        minimizer: [ new OptimizeCssAssetsPlugin()]
+
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false
+
+                },
+
+
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            // filename: '[name].[contentHash].css', // para produccion
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/assets', to: 'assets/'},
+            ],
+           
+        }),
+    ]
+
+
+
+
+};
+
